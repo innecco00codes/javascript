c683f195f78dc42e52244122f23864978229df32
Carlos Innecco <innecco00codes@gmail.com>
2023-02-12 10:54:02 +0100

    variables

--- hola-mundo.js
+++ hola-mundo.js
@@ -1,3 +1,10 @@
-alert("Bienvenidos")
-alert(">Esta es mi primer JavaScript")
-document.write("P-p@`s")
+/*
+alert("Bienvenidos");
+alert(">Esta es mi primer JavaScript");*/
+
+//document.write("P-p@`s");
+
+var pais = "Venezuela";
+var continente = "SurAmerica";
+
+console.log(pais, continente);
