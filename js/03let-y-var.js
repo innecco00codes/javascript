b83abb5c82be8f003e075a992760195e7856f194
Carlos Innecco <innecco00codes@gmail.com>
2023-02-13 19:37:16 +0100

    let-y-var

--- js/03let-y-var.js
+++ js/03let-y-var.js
@@ -1,3 +1,23 @@
 'use strict'
 
+//PRUEBA CON VAR
+var numero = 40; // valor 40
+console.log ( numero )
 
+if ( true ){
+    var numero = 50;
+    console.log ( numero ); // valor 50
+}
+
+console.log ( numero ) ; // valor 50
+
+//PRUEBA CON LET
+var texto = "Este es mi primer blog";
+console.log(texto); // valor =
+
+if(true){
+    let texto = "Ahora si comenzamos";
+    console.log(texto);
+}
+
+console.log(texto);
