4786669caafa257e96a0f9d1ef7b9b20ee0f1b66
Carlos Innecco <innecco00codes@gmail.com>
2023-02-17 22:54:29 +0100

    operadores

--- js/05datos-y-operadores.js
+++ js/05datos-y-operadores.js
@@ -1,6 +1,16 @@
 'use strict'
 
-//OPERADORES
+//OPERADORES  +suma -resta *producto /cociente %resto division
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
 
+alert("Es resultado de la operacón es: "+operacion);
 
 //TIPOS DE DATOS
+var numero_entero = 44;
+var cadena_texto = "Hola que tal";
+var verdadero_o_falso = true; //true o false, 0 O 1 VARIABLE BULEANO
+
+//FUNCION: PARA CAMBIAR UN DATO A OTRO
+
