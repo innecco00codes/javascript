1a5a00639d68d81d6304900153b4ff3656dc4e36
Carlos Innecco <innecco00codes@gmail.com>
2023-02-17 23:21:05 +0100

    funcion

--- js/05datos-y-operadores.js
+++ js/05datos-y-operadores.js
@@ -14,3 +14,9 @@ var verdadero_o_falso = true; //true o false, 0 O 1 VARIABLE BULEANO
 
 //FUNCION: PARA CAMBIAR UN DATO A OTRO
 
+var numero_falso = "33"; //es un string (cadena de caracteres)
+
+console.log(Number(numero_falso)+7); //Funcion Number cambia el valor del string a numero 
+console.log(parseInt(numero_falso)+7);
+console.log(parseFloat(numero_falso)+7); //numeros con decimales
+console.log(String(numero_entero)+4);// String cambia el valor de numero a texto
