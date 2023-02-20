11184b17192b239295d1b041c9a2f2ec59a6cc98
Carlos Innecco <innecco00codes@gmail.com>
2023-02-20 19:34:01 +0100

    condicionales

--- js/06condicionales.js
+++ js/06condicionales.js
@@ -54,4 +54,15 @@ if(year != 2016){
 }
 
 //AND
-if(year>= 2000 //)
+if(year>= 2000 && year <= 2020 && year != 2018){
+    console.log("Estamos en la era actual");
+}else{
+    console.log("Estamos en la era post moderna");
+}
+
+//OR
+if(year == 2008 || (year >= 2018 && year == 2028)){
+    console.log("El año acaba en 8");
+}else{
+    console.log("Año no registrado");
+}
