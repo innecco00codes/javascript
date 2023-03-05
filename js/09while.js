8e69c6fdc8c21703cc8acbbf22ea73345ac344c8
Carlos Innecco <innecco00codes@gmail.com>
2023-03-05 09:26:38 +0100

    WHILE

---
+++ js/09while.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//BUCLE WHILE ES UN BUCLE INDEFINIDO
+
+var year = 2018;
+
+while ( year <= 2051){
+    //Ejecuta esto
+    console.log("Estamos en el año " + year);
+    year++; //IMPORTANTE CONDICIONAR PARA QUE NO SEA UN BUCLE INFINITO
+}
+
+while(year != 1991){
+    //Ejecuta esto
+    console.log("Estamos en el año " + year);
+    year--;
+}
