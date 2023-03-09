50b5d1523e794b84e5f3802acbfd4950a537fa8b
Carlos Innecco <innecco00codes@gmail.com>
2023-03-09 06:49:25 +0100

    ventanas

--- js/09while.js
+++ js/09while.js
@@ -13,5 +13,23 @@ while ( year <= 2051){
 while(year != 1991){
     //Ejecuta esto
     console.log("Estamos en el año " + year);
+    
+    if(year == 2000){
+        break;
+    }
+
     year--;
+
 }
+
+
+
+//DO WHILE
+/*
+var years = 30;
+
+do{
+    alert("SOLO CUANDO SEA DIFERENTE A 20");
+    years--;
+
+}while(years > 25)*/
