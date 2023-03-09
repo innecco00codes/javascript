ae2fa1f5cbb5325d69b1316501f369dfd6d8321d
Carlos Innecco <innecco00codes@gmail.com>
2023-03-09 07:03:32 +0100

    ventana

---
+++ js/10ventanas.js
@@ -0,0 +1,16 @@
+'use stric'
+
+//VENTANA ALERTA
+
+alert ("Bienvenidos a este blog");
+
+//VENTANA ALERTA DE CONFIRMACION
+
+var miResultado = confirm ("¿Seguro de querer continuar?");
+console.log(miResultado);
+
+//VENTANA INGRESO INFORMACION O DATOS
+
+var mi_resultado = prompt("¿Qué edad tienes?", 18);
+console.log(mi_resultado);
+
