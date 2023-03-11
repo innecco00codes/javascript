f6d3629b9e83ae9ddf24f01d45d61eac7e6aa91a
Carlos Innecco <innecco00codes@gmail.com>
2023-03-11 06:44:29 +0100

    4

--- js/13ejercicio3.js
+++ js/13ejercicio3.js
@@ -7,4 +7,8 @@ CREAR UN PROGRAMA QUE MUESTRE TODOS LOS NUMEROS ENTRE DOS NUMEROS INTRODUCIDOS P
 var numero1 = parseInt(prompt("Introduce el primer número", 0));
 var numero2 = parseInt(prompt("Introduce el segundo nümero", 0));
 
-for( var i = numero1; i <= numero2; i++)
+document.write("<h1>De "+numero1+" a "+numero2+" estan estos numeros</h1>");
+
+for( var i = numero1; i <= numero2; i++){
+    document.write(i+"</br>");
+}
