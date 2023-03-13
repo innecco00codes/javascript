28c3724b55c1a1c0427af8078650059f166b2f51
Carlos Innecco <innecco00codes@gmail.com>
2023-03-13 07:31:13 +0100

    calculadora

---
+++ js/18ejercicio8.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/* CALCULADORA :
+- PIDA 2 NUMEROS POR PANTALLA
+- SI METEMOS UNO MAL, QUE LO VUELVA A PEDIR
+- EN EL CUERPO DE LA PAGIINA, EN UNA ALERTA Y POR CONSOLA EL RESULTADO DE LAS 2 CIFRAS
+
+*/
+
+var numero1 = parseInt(prompt("Introduce el primer número", 0));
+var numero2 = parseInt(prompt("Introduce el segundo número", 0));
+
+while (numero1 < 0 || numero2 < 0 || isNaN(numero1) || isNaN(numero2)){
+    numero1 = parseInt(prompt("Introduce el primer número", 0));
+    numero2 = parseInt(prompt("Introduce el segundo número", 0));
+}
+var resultado = "La suma es: " + (numero1 + numero2) + "<br/>" + 
+                "La resta es: " + (numero1 - numero2) + "<br/>" + 
+                "La multiplicacion es :" + (numero1 * numero2)+"<br/>"+
+                "La división es :" + (numero1 / numero2) + "<br/>";
+
+var resultado = "La suma es: " + (numero1 + numero2) + "<br/>" + 
+                "La resta es: " + (numero1 - numero2) + "<br/>" + 
+                "La multiplicacion es :" + (numero1 * numero2)+"<br/>"+
+                "La división es :" + (numero1 / numero2) + "<br/>";
+
+document.write(resultado);
