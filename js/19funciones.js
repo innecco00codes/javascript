909ba133f3b1238ed753beef39a7e923f4872129
Carlos Innecco <innecco00codes@gmail.com>
2023-03-14 07:17:31 +0100

    funciones

---
+++ js/19funciones.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// FUNCIONES : ES UNA AGRUPACION REUTILIZABLE DE UN CONJUNTO DE INSTRUCCIONES
+
+//Defino la funciones
+function calculadora(){
+
+    //Conjunto de instrucciones a ejecutar
+    return "Hola soy la calculadora";
+   
+}
+
+//Invocar o llamar la funcion.
+
+calculadora();
+console.log(calculadora);
+
+
+
+
