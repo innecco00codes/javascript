1a273b8cd076fdaf2db2eceb82f538e96735ffac
Carlos Innecco <innecco00codes@gmail.com>
2023-03-17 07:29:30 +0100

    parametros

--- js/19funciones.js
+++ js/19funciones.js
@@ -3,30 +3,36 @@
 // FUNCIONES : ES UNA AGRUPACION REUTILIZABLE DE UN CONJUNTO DE INSTRUCCIONES
 
 //Defino la funciones
+
+function porConsola(numero1, numero2){
+  console.log("Suma: " + (numero1 + numero2));
+  console.log("Resta: " + (numero1 - numero2));
+  console.log("Multiplicacion: " + (numero1 * numero2));
+  console.log("Division: " + (numero1 / numero2));
+  console.log("*****************************");
+}
+
+function porPantalla(numero1, numero2){
+  document.write("Suma: " + (numero1 + numero2) + "<br/>");
+  document.write("Resta: " + (numero1 - numero2) + "<br/>");
+  document.write("Multiplicacion: " + (numero1 * numero2) + "<br/>");
+  document.write("Division: " + (numero1 / numero2) + "<br/>");
+  document.write("*****************************") + "<br/>";
+}
 function calculadora(numero1, numero2, mostrar = false){
-
   if(mostrar == false){
-
-    console.log("Suma: " + (numero1 + numero2));
-    console.log("Resta: " + (numero1 - numero2));
-    console.log("Multiplicacion: " + (numero1 * numero2));
-    console.log("Division: " + (numero1 / numero2));
-    console.log("*****************************");
+    porConsola(numero1, numero2);  
   }else{
-    document.write("Suma: " + (numero1 + numero2) + "<br/>");
-    document.write("Resta: " + (numero1 - numero2) + "<br/>");
-    document.write("Multiplicacion: " + (numero1 * numero2) + "<br/>");
-    document.write("Division: " + (numero1 / numero2) + "<br/>");
-    document.write("*****************************") + "<br/>";  
+    porPantalla(numero1, numero2);
   }
 
-    return "Hola soy la calculadora";
+  return true;
    
 }
 
 //INVOCAR O LLAMAR LA FUNCION
 calculadora(5,5);
-calculadora(3,3, true);
+calculadora(3,5, true);
 
 /*
 for(var i = 1; i <= 10; i++){
