f57426f67712fa1edb647a10724e1be68931acd1
Carlos Innecco <innecco00codes@gmail.com>
2023-03-17 07:00:58 +0100

    calculadora

--- js/19funciones.js
+++ js/19funciones.js
@@ -9,10 +9,20 @@ function calculadora(numero1, numero2){
     console.log("Resta: " + (numero1 - numero2));
     console.log("Multiplicacion: " + (numero1 * numero2));
     console.log("Division: " + (numero1 / numero2));
+    console.log("*****************************");
+
+    return "Hola soy la calculadora";
    
 }
 
+//INVOCAR O LLAMAR LA FUNCION
 calculadora(5,5);
+calculadora(9,3);
+
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+    calculadora(i,8);
+}
 
 
 
