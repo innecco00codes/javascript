b2cdc8803af2bf669d77d27a74448128e37cbfab
Carlos Innecco <innecco00codes@gmail.com>
2023-03-17 07:14:26 +0100

    ejercicio

--- js/19funciones.js
+++ js/19funciones.js
@@ -3,13 +3,22 @@
 // FUNCIONES : ES UNA AGRUPACION REUTILIZABLE DE UN CONJUNTO DE INSTRUCCIONES
 
 //Defino la funciones
-function calculadora(numero1, numero2){
+function calculadora(numero1, numero2, mostrar = false){
+
+  if(mostrar == false){
 
     console.log("Suma: " + (numero1 + numero2));
     console.log("Resta: " + (numero1 - numero2));
     console.log("Multiplicacion: " + (numero1 * numero2));
     console.log("Division: " + (numero1 / numero2));
     console.log("*****************************");
+  }else{
+    document.write("Suma: " + (numero1 + numero2) + "<br/>");
+    document.write("Resta: " + (numero1 - numero2) + "<br/>");
+    document.write("Multiplicacion: " + (numero1 * numero2) + "<br/>");
+    document.write("Division: " + (numero1 / numero2) + "<br/>");
+    document.write("*****************************") + "<br/>";  
+  }
 
     return "Hola soy la calculadora";
    
@@ -17,13 +26,14 @@ function calculadora(numero1, numero2){
 
 //INVOCAR O LLAMAR LA FUNCION
 calculadora(5,5);
-calculadora(9,3);
+calculadora(3,3, true);
 
+/*
 for(var i = 1; i <= 10; i++){
-    console.log(i);
-    calculadora(i,8);
+  console.log(i);
+  calculadora(i,8);
 }
-
+*/
 
 
 
