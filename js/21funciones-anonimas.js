a5fddf124cb9140172ac48599d2bb87cdeb19c54
Carlos Innecco <innecco00codes@gmail.com>
2023-03-18 07:25:06 +0100

    anonimas

---
+++ js/21funciones-anonimas.js
@@ -0,0 +1,20 @@
+'use stritc'
+
+/*
+FUNCIONES ANONIMAS:
+ES UNA FUNCION QUE NO TIENE NOMBRE
+*/
+
+var peliculas = function (nombre){
+  return "La pelicula es: " + nombre; 
+}
+
+console.log(peliculas ("Harry Potter"));
+
+
+function sumame(numero1, numero2){
+  var sumar = (numero1 + numero2);
+  return sumar;
+}
+
+console.log(sumame(4,5));
