2bf8be67a32878cef288e3e0c2f869078f91bc6f
Carlos Innecco <innecco00codes@gmail.com>
2023-03-20 06:48:17 +0100

    funciones

--- js/21funciones-anonimas.js
+++ js/21funciones-anonimas.js
@@ -13,9 +13,20 @@ console.log(peliculas ("Harry Potter"));
 */
 
 // FUNCIONES CALLBACK
-function sumame(numero1, numero2){
+function sumame(numero1, numero2, sumaYmuestra, sumaPorDos){
   var sumar = (numero1 + numero2);
+
+  sumaYmuestra(sumar);
+  sumaPorDos(sumar);
+
   return sumar;
 }
 
-console.log(sumame(4,5));
+//console.log(sumame(4,5));
+
+sumame(5,7,function(dato){
+  console.log("La suma es: " + (dato));
+},
+function(dato){
+  console.log("La suma por dos es: " + (dato * 2));
+});
