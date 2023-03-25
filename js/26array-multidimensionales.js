831119e6bea5e654a9289c43cf149809a884c8cf
Carlos Innecco <innecco00codes@gmail.com>
2023-03-25 09:52:31 +0100

    multi

--- js/26array-multidimensionales.js
+++ js/26array-multidimensionales.js
@@ -1,2 +1,21 @@
 'use strict'
 
+// SON ARRAYS DENTRO DE OTRO ARRAY
+
+var peliculas = ["Harry Potter", "stars wars", "Scream", "Cars"];
+var categorias = ["Futurista", "Terror", "Animacion", "Fantasticas"];
+
+var cine = [categorias, peliculas];
+
+console.log(cine[0][3]);
+//               0 categorias
+//               3 fantasticas
+
+console.log(cine[1][0]);
+
+console.log(cine);
+
+//AÑADIR ELEMENTOS A UN ARRAY
+
+peliculas.push("Superman");
+console.log(peliculas);
