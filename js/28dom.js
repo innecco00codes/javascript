8d11877e301e5980055d0fced9165e6606bc0895
Carlos Innecco <innecco00codes@gmail.com>
2023-04-10 23:24:45 +0200

    dom2

--- js/28dom.js
+++ js/28dom.js
@@ -6,6 +6,8 @@ function cambiaColor(color){
     caja.style.background = color;
 };
 
+//CONSEGUIR ELEMENTOS CON UN ID CONCRETO
+
 //var caja = document.getElementById("micaja");
 
 var caja = document.querySelector("#micaja");
@@ -17,4 +19,14 @@ caja.style.color = "white";
 caja.className = "Hola";
 
 console.log(caja);
+
+//CONSEGUIR ELEMENTOS POR SU ETIQUETA 
+var todosLosDivs = document.getElementsByTagName('div');
+
+var contenidoEnTexto = todosLosDivs[2];
+contenidoEnTexto.innerHTML = "Otro texto para el segundo elemento";//SE MANIPULA EL TEXTO DE LA PAGINA
+
+console.log(todosLosDivs);
+
+//CONSEGUIR ELEMENTOS POR SU CLASE CSS
 
