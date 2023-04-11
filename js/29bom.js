f69b08a536c7160f2f280f82e4b77ae552aa290d
Carlos Innecco <innecco00codes@gmail.com>
2023-04-11 07:02:40 +0200

    bom

---
+++ js/29bom.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// BOM - BROWSER OBJECT MODEL
+
+console.log(window.innerWidth);
+console.log(innerHeight);
+console.log(screen.width);
+console.log(screen.height);
+console.log(window.location);
